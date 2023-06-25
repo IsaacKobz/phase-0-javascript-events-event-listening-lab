0212a347ac9250c66682af1a2513096239fcebce
Isaac <kobonyoisaac998@gmail.com>
2023-06-25 11:11:51 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+function addingEventListener(input) {
+    input = document.getElementById(`input`)
+    console.log(input)
+    input.addEventListener(`keypress`,addingEventListener)
+    input.innerText = `Now`
 }
+addingEventListener()
